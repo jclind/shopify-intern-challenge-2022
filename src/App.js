50dde11ac80d3477cfa53ff3cf1c36b37e305694
Jesse Lind <Jesseclind@gmail.com>
2022-05-20 21:15:05 -0400

    style: increase height of form input for better long-text visibility

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ function App() {
   const [promptHistory, setPromptHistory] = useState(() => {
     const promptHistoryLS = JSON.parse(localStorage.getItem('promptHistory'))
     if (promptHistoryLS) return promptHistoryLS
-
     return []
   })
 
